cb6b85a4f7b7dca3aa2836ab6cf1162ddb80e611
KanroMiku <843734139@qq.com>
2020-07-29 18:58:31 +0800

    Update Pei,change welcome

--- assets/search.js
+++ assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"裴语",'url':"/%E8%A3%B4%E8%AF%AD"},{'title':"暂时离开",'url':"/%E6%9A%82%E6%97%B6%E7%A6%BB%E5%BC%80"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"裴语",'url':"/%E8%A3%B4%E8%AF%AD"},{'title':"Welcome",'url':"/welcome"}]};
